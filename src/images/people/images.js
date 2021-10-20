22acb7744f7c362f515cb399f2ea8ce4c99d4c01
dewey <hammersdrichard@gmail.com>
2021-10-20 07:41:51 -0700

    pics add to all peoplr

--- src/images/people/images.js
+++ src/images/people/images.js
@@ -1,13 +1,496 @@
+import luke_skywalker from "./Luke-skywalker.jpg";
+import C3PO from "./C-3PO.png";
+import R2_D2 from "./R2-D2.png";
+import Darth_Varder from "./Darth-Vader.jpeg";
+import Leia_Organ from "./Leia-Organa.jpg";
+import Owen_lars from "./Owen-Lars.png";
+import Beru_Whitesun_lars from "./Beru-Whitesun-lars.jpg";
+import R5_D4 from "./R5-D4.png";
+import Biggs_Darklighter from "./Biggs-Darklighter.png";
+import Obi_Wan_Kenobi from "./Obi-Wan-Kenobi.jpg";
+import Anakin_Skywalker from "./Anakin-Skywalker.png";
+import Wilhuff_Tarkin from "./Wilhuff-Tarkin.jpg";
+import Chewbacca from "./Chewbacca.jpeg";
+import Han_Solo from "./Han-Solo.jpg";
+import Greedo from "./Greedo.jpg";
+import Jabba_Desilijic_Tiure from "./Jabba-Desilijic-Tiure.png";
+import Wedge_Antilles from "./Wedge-Antilles.png";
+import Jek_Tono_Porkins from "./Jek-Tono-Porkins.jpg";
+import Yoda from "./Yoda.png";
+import Palpatine from "./Palpatine.jpg";
+import Boba_Fett from "./Boba-Fett.jpg";
+import IG_88 from "./IG-88.jpg";
+import Bossk from "./Bossk.jpg";
+import Lando_Calrissian from "./Lando-Calrissian.jpg";
+import Lobot from "./Lobot.jpeg";
+import Ackbar from "./Ackbar.jpeg";
+import Mon_Mothma from "./Mon-Mothma.jpg";
+import Arvel_Crynyd from "./Arvel-Crynyd.jpg";
+import Wicket_Systri_Warrick from "./Wicket-Systri-Warrick.jpeg";
+import Nien_Nunb from "./Nien-Nunb.jpeg";
+import Qui_Gon_Jinn from "./Qui-Gon-Jinn.jpeg";
+import Nute_Gunray from "./Nute-Gunray.jpeg";
+import Finis_Valorum from "./Finis-Valorum.jpeg";
+import Padmé_Amidala from "./Padmé-Amidala.jpg";
+import Jar_Jar_Binks from "./Jar-Jar-Binks.jpeg";
+import Roos_Tarpals from "./Roos-Tarpals.jpg";
+import Rugor_Nass from "./Rugor-Nass.jpeg";
+import Ric_Olié from "./Ric-Olié.jpeg";
+import Watto from "./Watto.jpeg";
+import Sebulba from "./Sebulba.jpeg";
+import Quarsh_Panaka from "./Quarsh-Panaka.jpg";
+import Shmi_Skywalker from "./Shmi-Skywalker.png";
+import Darth_Maul from "./Darth-Maul.jpeg";
+import Bib_Fortuna from "./Bib-Fortuna.png";
+import Ayla_Secura from "./Ayla-Secura.jpg";
+import Ratts_Tyerel from "./Ratts-Tyerel.jpeg";
+import Dud_Bolt from "./Dud-Bolt.png";
+import Gasgano from "./Gasgano.jpeg";
+import Ben_Quadinaros from "./Ben-Quadinaros.jpeg";
+import Mace_Windu from "./Mace-Windu.jpg";
+import Ki_Adi_Mundi from "./Ki-Adi-Mundi.jpeg";
+import Kit_Fisto from "./Kit-Fisto.jpeg";
+import Eeth_Koth from "./Eeth-Koth.jpeg";
+import Adi_Gallia from "./Adi-Gallia.jpeg";
+import Saesee_Tiin from "./Saesee-Tiin.png";
+import Yarael_Poof from "./Yarael-Poof.png";
+import Plo_Koon from "./Plo-Koon.jpeg";
+import Mas_Amedda from "./Mas-Amedda.jpg";
+import Gregar_Typho from "./Gregar-Typho.jpeg";
+import Cordé from "./Cordé.jpg";
+import Cliegg_Lars from "./Cliegg-Lars.jpeg";
+import Poggle_the_Lesser from "./Poggle-the-Lesser.jpeg";
+import Luminara_Unduli from "./Luminara-Unduli.png";
+import Barriss_Offee from "./Barriss-Offee.jpeg";
+import Dormé from "./Dormé.jpg";
+import Dooku from "./Dooku.jpg";
+import Bail_Prestor_Organa from "./Bail-Prestor-Organa.jpg";
+import Jango_Fett from "./Jango-Fett.jpeg";
+import Zam_Wesell from "./Zam-Wesell.jpg";
+import Dexter_Jettster from "./Dexter-Jettster.jpeg";
+import Lama_Su from "./Lama-Su.png";
+import Taun_We from "./Taun-We.jpg";
+import Jocasta_Nu from "./Jocasta-Nu.jpeg";
+import R4_P17 from "./R4-P17.jpg";
+import Wat_Tambor from "./Wat-Tambor.jpeg";
+import San_Hill from "./San-Hill.jpeg";
+import Shaak_Ti from "./Shaak-Ti.jpg";
+import Grievous from "./Grievous.jpeg";
+import Tarfful from "./Tarfful.jpeg";
+import Raymus_Antilles from "./Raymus-Antilles.png";
+import Sly_Moore from "./Sly-Moore.jpeg";
+import Tion_Medon from "./Tion-Medon.jpg";
+
 const images = [
   {
     id: 1,
-    src: "/static/media/Luke-skywalker.a43b1d72.jpg",
+    src: luke_skywalker,
     title: "Luke Skywalker",
   },
   {
     id: 2,
-    src: "/static/media/C-3PO.ca8697e9.png",
+    src: C3PO,
     title: "C-3PO",
+  },
+  {
+    id: 3,
+    src: R2_D2,
+    title: "R2-D2",
+  },
+  {
+    id: 4,
+    src: Darth_Varder,
+    title: "Darth Vader",
+  },
+  {
+    id: 5,
+    src: Leia_Organ,
+    title: "Leia Organa",
+  },
+  {
+    id: 6,
+    src: Owen_lars,
+    title: "Owen Lars",
+  },
+  {
+    id: 7,
+    src: Beru_Whitesun_lars,
+    title: "Beru Whitesun lars",
+  },
+  {
+    id: 8,
+    src: R5_D4,
+    title: "R5-D4",
+  },
+  {
+    id: 9,
+    src: Biggs_Darklighter,
+    title: "Biggs Darklighter",
+  },
+  {
+    id: 10,
+    src: Obi_Wan_Kenobi,
+    title: "Obi-Wan Kenobi",
+  },
+  {
+    id: 11,
+    src: Anakin_Skywalker,
+    title: "Anakin Skywalker",
+  },
+  {
+    id: 12,
+    src: Wilhuff_Tarkin,
+    title: "Wilhuff Tarkin",
+  },
+  {
+    id: 13,
+    src: Chewbacca,
+    title: "Chewbacca",
+  },
+  {
+    id: 14,
+    src: Han_Solo,
+    title: "Han Solo",
+  },
+  {
+    id: 15,
+    src: Greedo,
+    title: "Greedo",
+  },
+  {
+    id: 16,
+    src: Jabba_Desilijic_Tiure,
+    title: "Jabba Desilijic Tiure",
+  },
+  {
+    id: 18,
+    src: Wedge_Antilles,
+    title: "Wedge Antilles",
+  },
+  {
+    id: 19,
+    src: Jek_Tono_Porkins,
+    title: "Jek Tono Porkins",
+  },
+  {
+    id: 20,
+    src: Yoda,
+    title: "Yoda",
+  },
+  {
+    id: 21,
+    src: Palpatine,
+    title: "Palpatine",
+  },
+  {
+    id: 22,
+    src: Boba_Fett,
+    title: "Boba Fett",
+  },
+  {
+    id: 23,
+    src: IG_88,
+    title: "IG-88",
+  },
+  {
+    id: 24,
+    src: Bossk,
+    title: "Bossk",
+  },
+  {
+    id: 25,
+    src: Lando_Calrissian,
+    title: "Lando Calrissian",
+  },
+  {
+    id: 26,
+    src: Lobot,
+    title: "Lobot",
+  },
+  {
+    id: 27,
+    src: Ackbar,
+    title: "Ackbar",
+  },
+  {
+    id: 28,
+    src: Mon_Mothma,
+    title: "Mon Mothma",
+  },
+  {
+    id: 29,
+    src: Arvel_Crynyd,
+    title: "Arvel Crynyd",
+  },
+  {
+    id: 30,
+    src: Wicket_Systri_Warrick,
+    title: "Wicket Systri Warrick",
+  },
+  {
+    id: 31,
+    src: Nien_Nunb,
+    title: "Nien Nunb",
+  },
+  {
+    id: 32,
+    src: Qui_Gon_Jinn,
+    title: "Qui-Gon Jinn",
+  },
+  {
+    id: 33,
+    src: Nute_Gunray,
+    title: "Nute Gunray",
+  },
+  {
+    id: 34,
+    src: Finis_Valorum,
+    title: "Finis Valorum",
+  },
+  {
+    id: 35,
+    src: Padmé_Amidala,
+    title: "Padmé Amidala",
+  },
+  {
+    id: 36,
+    src: Jar_Jar_Binks,
+    title: "Jar Jar Binks",
+  },
+  {
+    id: 37,
+    src: Roos_Tarpals,
+    title: "Roos Tarpals",
+  },
+  {
+    id: 38,
+    src: Rugor_Nass,
+    title: "Rugor Nass",
+  },
+  {
+    id: 39,
+    src: Ric_Olié,
+    title: "Ric Olié",
+  },
+  {
+    id: 40,
+    src: Watto,
+    title: "Watto",
+  },
+  {
+    id: 41,
+    src: Sebulba,
+    title: "Sebulba",
+  },
+  {
+    id: 42,
+    src: Quarsh_Panaka,
+    title: "Quarsh Panaka",
+  },
+  {
+    id: 43,
+    src: Shmi_Skywalker,
+    title: "Shmi Skywalker",
+  },
+  {
+    id: 44,
+    src: Darth_Maul,
+    title: "Darth Maul",
+  },
+  {
+    id: 45,
+    src: Bib_Fortuna,
+    title: "Bib Fortuna",
+  },
+  {
+    id: 46,
+    src: Ayla_Secura,
+    title: "Ayla Secura",
+  },
+  {
+    id: 47,
+    src: Ratts_Tyerel,
+    title: "Ratts Tyerel",
+  },
+  {
+    id: 48,
+    src: Dud_Bolt,
+    title: "Dud Bolt",
+  },
+  {
+    id: 49,
+    src: Gasgano,
+    title: "Gasgano",
+  },
+  {
+    id: 50,
+    src: Ben_Quadinaros,
+    title: "Ben Quadinaros",
+  },
+  {
+    id: 51,
+    src: Mace_Windu,
+    title: "Mace Windu",
+  },
+  {
+    id: 52,
+    src: Ki_Adi_Mundi,
+    title: "Ki-Adi-Mundi",
+  },
+  {
+    id: 53,
+    src: Kit_Fisto,
+    title: "Kit Fisto",
+  },
+  {
+    id: 54,
+    src: Eeth_Koth,
+    title: "Eeth Koth",
+  },
+  {
+    id: 55,
+    src: Adi_Gallia,
+    title: "Adi Gallia",
+  },
+  {
+    id: 56,
+    src: Saesee_Tiin,
+    title: "Saesee Tiin",
+  },
+  {
+    id: 57,
+    src: Yarael_Poof,
+    title: "Yarael Poof",
+  },
+  {
+    id: 58,
+    src: Plo_Koon,
+    title: "Plo Koon",
+  },
+  {
+    id: 59,
+    src: Mas_Amedda,
+    title: "Mas Amedda",
+  },
+  {
+    id: 60,
+    src: Gregar_Typho,
+    title: "Gregar Typho",
+  },
+  {
+    id: 61,
+    src: Cordé,
+    title: "Cordé",
+  },
+  {
+    id: 62,
+    src: Cliegg_Lars,
+    title: "Cliegg Lars",
+  },
+  {
+    id: 63,
+    src: Poggle_the_Lesser,
+    title: "Poggle the Lesser",
+  },
+  {
+    id: 64,
+    src: Luminara_Unduli,
+    title: "Luminara Unduli",
+  },
+  {
+    id: 65,
+    src: Barriss_Offee,
+    title: "Barriss Offee",
+  },
+  {
+    id: 66,
+    src: Dormé,
+    title: "Dormé",
+  },
+  {
+    id: 67,
+    src: Dooku,
+    title: "Dooku",
+  },
+  {
+    id: 68,
+    src: Bail_Prestor_Organa,
+    title: "Bail Prestor Organa",
+  },
+  {
+    id: 69,
+    src: Jango_Fett,
+    title: "Jango Fett",
+  },
+  {
+    id: 70,
+    src: Zam_Wesell,
+    title: "Zam Wesell",
+  },
+  {
+    id: 71,
+    src: Dexter_Jettster,
+    title: "Dexter Jettster",
+  },
+  {
+    id: 72,
+    src: Lama_Su,
+    title: "Lama Su",
+  },
+  {
+    id: 73,
+    src: Taun_We,
+    title: "Taun We",
+  },
+  {
+    id: 74,
+    src: Jocasta_Nu,
+    title: "Jocasta Nu",
+  },
+  {
+    id: 75,
+    src: R4_P17,
+    title: "R4-P17",
+  },
+  {
+    id: 76,
+    src: Wat_Tambor,
+    title: "Wat Tambor",
+  },
+  {
+    id: 77,
+    src: San_Hill,
+    title: "San Hill",
+  },
+  {
+    id: 78,
+    src: Shaak_Ti,
+    title: "Shaak Ti",
+  },
+  {
+    id: 79,
+    src: Grievous,
+    title: "Grievous",
+  },
+  {
+    id: 80,
+    src: Tarfful,
+    title: "Tarfful",
+  },
+  {
+    id: 81,
+    src: Raymus_Antilles,
+    title: "Raymus Antilles",
+  },
+  {
+    id: 82,
+    src: Sly_Moore,
+    title: "Sly Moore",
+  },
+  {
+    id: 83,
+    src: Tion_Medon,
+    title: "Tion Medon",
   },
 ];
 
