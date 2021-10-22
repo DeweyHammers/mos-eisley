8cf7de66fdac51f12ede05f95c33b47da2a48687
dewey <hammersdrichard@gmail.com>
2021-10-22 05:11:00 -0700

    add images to all vehicles

--- src/images/vehicles/images.js
+++ src/images/vehicles/images.js
@@ -1,159 +1,199 @@
+import Sand_Crawler from "./Sand_Crawler.png";
+import T_16_skyhopper from "./T_16_skyhopper.png";
+import X_34_landspeeder from "./X_34_landspeeder.jpeg";
+import TIE_LN_starfighter from "./TIE_LN_starfighter.png";
+import Snowspeeder from "./Snowspeeder.jpeg";
+import TIE_bomber from "./TIE_bomber.jpeg";
+import AT_AT from "./AT_AT.png";
+import AT_ST from "./AT_ST.png";
+import Storm_IV_Twin_Pod_cloud_car from "./Storm_IV_Twin_Pod_cloud_car.png";
+import Sail_barge from "./Sail_barge.png";
+import Bantha_II_cargo_skiff from "./Bantha_II_cargo_skiff.jpg";
+import TIE_IN_interceptor from "./TIE_IN_interceptor.jpg";
+import Imperial_Speeder_Bike from "./Imperial_Speeder_Bike.png";
+import Vulture_Droid from "./Vulture_Droid.jpeg";
+import Multi_Troop_Transport from "./Multi_Troop_Transport.png";
+import Armored_Assault_Tank from "./Armored_Assault_Tank.png";
+import Single_Trooper_Aerial_Platform from "./Single_Trooper_Aerial_Platform.png";
+import C_9979_landing_craft from "./C_9979_landing_craft.jpg";
+import Tribubble_bongo from "./Tribubble_bongo.jpg";
+import Sith_speeder from "./Sith_speeder.png";
+import Zephyr_G_swoop_bike from "./Zephyr_G_swoop_bike.jpg";
+import Koro_2_Exodrive_airspeeder from "./Koro_2_Exodrive_airspeeder.jpg";
+import XJ_6_airspeeder from "./XJ_6_airspeeder.jpg";
+import LAAT_i from "./LAAT_i.png";
+import LAAT_c from "./LAAT_c.png";
+import AT_TE from "./AT_TE.jpg";
+import SPHA from "./SPHA.jpg";
+import Flitknot_speeder from "./Flitknot_speeder.jpg";
+import Neimoidian_shuttle from "./Neimoidian_shuttle.jpg";
+import Geonosian_starfighter from "./Geonosian_starfighter.png";
+import Tsmeu_6_personal_wheel_bike from "./Tsmeu_6_personal_wheel_bike.jpg";
+import Emergency_Firespeeder from "./Emergency_Firespeeder.jpg";
+import Droid_tri_fighter from "./Droid_tri_fighter.png";
+import Oevvaor_jet_catamaran from "./Oevvaor_jet_catamaran.jpg";
+import Raddaugh_Gnasp_fluttercraft from "./Raddaugh_Gnasp_fluttercraft.jpg";
+import Clone_turbo_tank from "./Clone_turbo_tank.png";
+import Corporate_Alliance_tank_droid from "./Corporate_Alliance_tank_droid.png";
+import Droid_gunship from "./Droid_gunship.png";
+import AT_RT from "./AT_RT.png";
+
 const images = {
   "Sand Crawler": {
     id: 1,
-    src: "",
+    src: Sand_Crawler,
   },
   "T-16 skyhopper": {
     id: 2,
-    src: "",
+    src: T_16_skyhopper,
   },
   "X-34 landspeeder": {
     id: 3,
-    src: "",
+    src: X_34_landspeeder,
   },
   "TIE/LN starfighter": {
     id: 4,
-    src: "",
+    src: TIE_LN_starfighter,
   },
   Snowspeeder: {
     id: 5,
-    src: "",
+    src: Snowspeeder,
   },
   "TIE bomber": {
     id: 6,
-    src: "",
+    src: TIE_bomber,
   },
   "AT-AT": {
     id: 7,
-    src: "",
+    src: AT_AT,
   },
   "AT-ST": {
     id: 8,
-    src: "",
+    src: AT_ST,
   },
   "Storm IV Twin-Pod cloud car": {
     id: 9,
-    src: "",
+    src: Storm_IV_Twin_Pod_cloud_car,
   },
   "Sail barge": {
     id: 10,
-    src: "",
+    src: Sail_barge,
   },
   "Bantha-II cargo skiff": {
     id: 11,
-    src: "",
+    src: Bantha_II_cargo_skiff,
   },
   "TIE/IN interceptor": {
     id: 12,
-    src: "",
+    src: TIE_IN_interceptor,
   },
   "Imperial Speeder Bike": {
     id: 13,
-    src: "",
+    src: Imperial_Speeder_Bike,
   },
   "Vulture Droid": {
     id: 14,
-    src: "",
+    src: Vulture_Droid,
   },
   "Multi-Troop Transport": {
     id: 15,
-    src: "",
+    src: Multi_Troop_Transport,
   },
   "Armored Assault Tank": {
     id: 16,
-    src: "",
+    src: Armored_Assault_Tank,
   },
   "Single Trooper Aerial Platform": {
     id: 17,
-    src: "",
+    src: Single_Trooper_Aerial_Platform,
   },
   "C-9979 landing craft": {
     id: 18,
-    src: "",
+    src: C_9979_landing_craft,
   },
   "Tribubble bongo": {
     id: 19,
-    src: "",
+    src: Tribubble_bongo,
   },
   "Sith speeder": {
     id: 20,
-    src: "",
+    src: Sith_speeder,
   },
   "Zephyr-G swoop bike": {
     id: 21,
-    src: "",
+    src: Zephyr_G_swoop_bike,
   },
   "Koro-2 Exodrive airspeeder": {
     id: 22,
-    src: "",
+    src: Koro_2_Exodrive_airspeeder,
   },
   "XJ-6 airspeeder": {
     id: 23,
-    src: "",
+    src: XJ_6_airspeeder,
   },
   "LAAT/i": {
     id: 24,
-    src: "",
+    src: LAAT_i,
   },
   "LAAT/c": {
     id: 25,
-    src: "",
+    src: LAAT_c,
   },
   "AT-TE": {
     id: 26,
-    src: "",
+    src: AT_TE,
   },
   SPHA: {
     id: 27,
-    src: "",
+    src: SPHA,
   },
   "Flitknot speeder": {
     id: 28,
-    src: "",
+    src: Flitknot_speeder,
   },
   "Neimoidian shuttle": {
     id: 29,
-    src: "",
+    src: Neimoidian_shuttle,
   },
   "Geonosian starfighter": {
     id: 30,
-    src: "",
+    src: Geonosian_starfighter,
   },
   "Tsmeu-6 personal wheel bike": {
     id: 31,
-    src: "",
+    src: Tsmeu_6_personal_wheel_bike,
   },
   "Emergency Firespeeder": {
     id: 32,
-    src: "",
+    src: Emergency_Firespeeder,
   },
   "Droid tri-fighter": {
     id: 33,
-    src: "",
+    src: Droid_tri_fighter,
   },
   "Oevvaor jet catamaran": {
     id: 34,
-    src: "",
+    src: Oevvaor_jet_catamaran,
   },
   "Raddaugh Gnasp fluttercraft": {
     id: 35,
-    src: "",
+    src: Raddaugh_Gnasp_fluttercraft,
   },
   "Clone turbo tank": {
     id: 36,
-    src: "",
+    src: Clone_turbo_tank,
   },
   "Corporate Alliance tank droid": {
     id: 37,
-    src: "",
+    src: Corporate_Alliance_tank_droid,
   },
   "Droid gunship": {
     id: 38,
-    src: "",
+    src: Droid_gunship,
   },
   "AT-RT": {
     id: 39,
-    src: "",
+    src: AT_RT,
   },
 };
 
