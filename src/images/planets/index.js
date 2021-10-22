d898d621e01bc2d14a81e6a4c6514a90223ce2f6
dewey <hammersdrichard@gmail.com>
2021-10-22 06:18:34 -0700

    add images to planets

--- src/images/planets/index.js
+++ src/images/planets/index.js
@@ -1,243 +1,304 @@
+import Tatooine from "./Tatooine.png";
+import Alderaan from "./Alderaan.jpg";
+import Yavin_IV from "./Yavin_IV.png";
+import Hoth from "./Hoth.png";
+import Dagobah from "./Dagobah.png";
+import Bespin from "./Bespin.png";
+import Endor from "./Endor.png";
+import Naboo from "./Naboo.png";
+import Coruscant from "./Coruscant.jpg";
+import Kamino from "./Kamino.jpg";
+import Geonosis from "./Geonosis.png";
+import Utapau from "./Utapau.png";
+import Mustafar from "./Mustafar.png";
+import Kashyyyk from "./Kashyyyk.png";
+import Polis_Massa from "./Polis_Massa.png";
+import Mygeeto from "./Mygeeto.jpg";
+import Felucia from "./Felucia.png";
+import Cato_Neimoidia from "./Cato_Neimoidia.png";
+import Saleucami from "./Saleucami.png";
+import Stewjon from "./Stewjon.png";
+import Eriadu from "./Eriadu.jpg";
+import Corellia from "./Corellia.png";
+import Rodia from "./Rodia.png";
+import Nal_Hutta from "./Nal_Hutta.png";
+import Dantooine from "./Dantooine.jpg";
+import Bestine_IV from "./Bestine_IV.png";
+import Ord_Mantell from "./Ord_Mantell.png";
+import unknown from "./unknown.jpg";
+import Trandosha from "./Trandosha.png";
+import Socorro from "./Socorro.png";
+import Mon_Cala from "./Mon_Cala.png";
+import Chandrila from "./Chandrila.png";
+import Sullust from "./Sullust.png";
+import Toydaria from "./Toydaria.png";
+import Malastare from "./Malastare.jpg";
+import Dathomir from "./Dathomir.png";
+import Ryloth from "./Ryloth.png";
+import Aleen_Minor from "./Aleen_Minor.jpg";
+import Vulpter from "./Vulpter.jpg";
+import Troiken from "./Troiken.jpg";
+import Tund from "./Tund.jpg";
+import Haruun_Kal from "./Haruun_Kal.png";
+import Cerea from "./Cerea.png";
+import Glee_Anselm from "./Glee_Anselm.jpg";
+import Iridonia from "./Iridonia.jpg";
+import Tholoth from "./Tholoth.jpg";
+import Iktotch from "./Iktotch.png";
+import Quermia from "./Quermia.jpg";
+import Dorin from "./Dorin.png";
+import Champala from "./Champala.jpg";
+import Mirial from "./Mirial.png";
+import Serenno from "./Serenno.png";
+import Concord_Dawn from "./Concord_Dawn.png";
+import Zolan from "./Zolan.jpg";
+import Ojom from "./Ojom.jpg";
+import Skako from "./Skako.png";
+import Muunilinst from "./Muunilinst.jpg";
+import Shili from "./Shili.jpg";
+import Kalee from "./Kalee.jpg";
+import Umbara from "./Umbara.jpg";
+
 const images = {
   Tatooine: {
     id: 1,
-    src: "",
+    src: Tatooine,
   },
   Alderaan: {
     id: 2,
-    src: "",
+    src: Alderaan,
   },
   "Yavin IV": {
     id: 3,
-    src: "",
+    src: Yavin_IV,
   },
   Hoth: {
     id: 4,
-    src: "",
+    src: Hoth,
   },
   Dagobah: {
     id: 5,
-    src: "",
+    src: Dagobah,
   },
   Bespin: {
     id: 6,
-    src: "",
+    src: Bespin,
   },
   Endor: {
     id: 7,
-    src: "",
+    src: Endor,
   },
   Naboo: {
     id: 8,
-    src: "",
+    src: Naboo,
   },
   Coruscant: {
     id: 9,
-    src: "",
+    src: Coruscant,
   },
   Kamino: {
     id: 10,
-    src: "",
+    src: Kamino,
   },
   Geonosis: {
     id: 11,
-    src: "",
+    src: Geonosis,
   },
   Utapau: {
     id: 12,
-    src: "",
+    src: Utapau,
   },
   Mustafar: {
     id: 13,
-    src: "",
+    src: Mustafar,
   },
   Kashyyyk: {
     id: 14,
-    src: "",
+    src: Kashyyyk,
   },
   "Polis Massa": {
     id: 15,
-    src: "",
+    src: Polis_Massa,
   },
   Mygeeto: {
     id: 16,
-    src: "",
+    src: Mygeeto,
   },
   Felucia: {
     id: 17,
-    src: "",
+    src: Felucia,
   },
   "Cato Neimoidia": {
     id: 18,
-    src: "",
+    src: Cato_Neimoidia,
   },
   Saleucami: {
     id: 19,
-    src: "",
+    src: Saleucami,
   },
   Stewjon: {
     id: 20,
-    src: "",
+    src: Stewjon,
   },
   Eriadu: {
     id: 21,
-    src: "",
+    src: Eriadu,
   },
   Corellia: {
     id: 22,
-    src: "",
+    src: Corellia,
   },
   Rodia: {
     id: 23,
-    src: "",
+    src: Rodia,
   },
   "Nal Hutta": {
     id: 24,
-    src: "",
+    src: Nal_Hutta,
   },
   Dantooine: {
     id: 25,
-    src: "",
+    src: Dantooine,
   },
   "Bestine IV": {
     id: 26,
-    src: "",
+    src: Bestine_IV,
   },
   "Ord Mantell": {
     id: 27,
-    src: "",
+    src: Ord_Mantell,
   },
   unknown: {
     id: 28,
-    src: "",
+    src: unknown,
   },
   Trandosha: {
     id: 29,
-    src: "",
+    src: Trandosha,
   },
   Socorro: {
     id: 30,
-    src: "",
+    src: Socorro,
   },
   "Mon Cala": {
     id: 31,
-    src: "",
+    src: Mon_Cala,
   },
   Chandrila: {
     id: 32,
-    src: "",
+    src: Chandrila,
   },
   Sullust: {
     id: 33,
-    src: "",
+    src: Sullust,
   },
   Toydaria: {
     id: 34,
-    src: "",
+    src: Toydaria,
   },
   Malastare: {
     id: 35,
-    src: "",
+    src: Malastare,
   },
   Dathomir: {
     id: 36,
-    src: "",
+    src: Dathomir,
   },
   Ryloth: {
     id: 37,
-    src: "",
+    src: Ryloth,
   },
   "Aleen Minor": {
     id: 38,
-    src: "",
+    src: Aleen_Minor,
   },
   Vulpter: {
     id: 39,
-    src: "",
+    src: Vulpter,
   },
   Troiken: {
     id: 40,
-    src: "",
+    src: Troiken,
   },
   Tund: {
     id: 41,
-    src: "",
+    src: Tund,
   },
   "Haruun Kal": {
     id: 42,
-    src: "",
+    src: Haruun_Kal,
   },
   Cerea: {
     id: 43,
-    src: "",
+    src: Cerea,
   },
   "Glee Anselm": {
     id: 44,
-    src: "",
+    src: Glee_Anselm,
   },
   Iridonia: {
     id: 45,
-    src: "",
+    src: Iridonia,
   },
   Tholoth: {
     id: 46,
-    src: "",
+    src: Tholoth,
   },
   Iktotch: {
     id: 47,
-    src: "",
+    src: Iktotch,
   },
   Quermia: {
     id: 48,
-    src: "",
+    src: Quermia,
   },
   Dorin: {
     id: 49,
-    src: "",
+    src: Dorin,
   },
   Champala: {
     id: 50,
-    src: "",
+    src: Champala,
   },
   Mirial: {
     id: 51,
-    src: "",
+    src: Mirial,
   },
   Serenno: {
     id: 52,
-    src: "",
+    src: Serenno,
   },
   "Concord Dawn": {
     id: 53,
-    src: "",
+    src: Concord_Dawn,
   },
   Zolan: {
     id: 54,
-    src: "",
+    src: Zolan,
   },
   Ojom: {
     id: 55,
-    src: "",
+    src: Ojom,
   },
   Skako: {
     id: 56,
-    src: "",
+    src: Skako,
   },
   Muunilinst: {
     id: 57,
-    src: "",
+    src: Muunilinst,
   },
   Shili: {
     id: 58,
-    src: "",
+    src: Shili,
   },
   Kalee: {
     id: 59,
-    src: "",
+    src: Kalee,
   },
   Umbara: {
     id: 60,
-    src: "",
+    src: Umbara,
   },
 };
 
